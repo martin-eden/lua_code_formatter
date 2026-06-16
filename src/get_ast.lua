1aafecb00457c0735ed9134b7198b39d4cef0950
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-16 15:06:33 +0200

    [~] [get_ast]

--- src/get_ast.lua
+++ src/get_ast.lua
@@ -1,8 +1,21 @@
+-- Parse Lua code and write syntax tree to file
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-16
+]]
+
+--[[ Develop
+package.path = package.path .. ';../../../?.lua'
+--]]
+require('workshop.base')
+
+-- Imports:
 local get_params = request('get_ast.get_params')
 local convert = request('!.file.convert')
 local get_ast = request('!.lua.code.get_ast')
 
-return
+local get_ast =
   function(args)
     local f_in_name, f_out_name = get_params(args)
     if not f_in_name then
@@ -16,3 +29,13 @@ return
       }
     )
   end
+
+-- Main:
+do
+  get_ast(_G.arg)
+end
+
+--[[
+  2017 # #
+  2026-06-16
+]]
